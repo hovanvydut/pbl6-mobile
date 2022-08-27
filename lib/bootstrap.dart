0b51c3720837da57eab05a2f602de28ef5f209dd
dungngminhh <63831488+dungngminh@users.noreply.github.com>
2022-08-27 16:09:13 +0700

    chore: reinit project

---
+++ lib/bootstrap.dart
@@ -0,0 +1,69 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    log('onBlocCreate -- ${bloc.runtimeType}', name: '${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log(
+      'onAddEvent -- ${bloc.runtimeType}, $event',
+      name: '${bloc.runtimeType}',
+    );
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log(
+      'onStateChange -- ${bloc.runtimeType}, $change',
+      name: '${bloc.runtimeType}',
+    );
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log(
+      'onStateTransition -- ${bloc.runtimeType}, $transition',
+      name: '${bloc.runtimeType}',
+    );
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError -- ${bloc.runtimeType}, $error', name: '${bloc.runtimeType}');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    log('onBlocClose -- ${bloc.runtimeType}', name: '${bloc.runtimeType}');
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  Bloc.observer = AppBlocObserver();
+
+  await runZonedGuarded(
+    () async => runApp(await builder()),
+    (error, stackTrace) =>
+        log(error.toString(), stackTrace: stackTrace, name: 'ERROR'),
+  );
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:pbl6_mobile/app/app.dart';
+import 'package:pbl6_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
